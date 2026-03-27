b69f07bcfe815fafa51e558d07b5fb11c52af4e6
maximum-commitment <batman@gotham.gov>
2026-03-27 12:32:14 -0400

    ignore files remove old build
    updated ignore

--- STM32CubeIDE/Debug/Drivers/BSP/Components/lis302dl.d
+++
@@ -1,6 +0,0 @@
-Drivers/BSP/Components/lis302dl.o: \
- /Users/charlesmurphy/STM32CubeIDE/workspace_1.18.0/Demonstrations/Drivers/BSP/Components/lis302dl/lis302dl.c \
- /Users/charlesmurphy/STM32CubeIDE/workspace_1.18.0/Demonstrations/Drivers/BSP/Components/lis302dl/lis302dl.h \
- /Users/charlesmurphy/STM32CubeIDE/workspace_1.18.0/Demonstrations/Drivers/BSP/Components/lis302dl/../Common/accelero.h
-/Users/charlesmurphy/STM32CubeIDE/workspace_1.18.0/Demonstrations/Drivers/BSP/Components/lis302dl/lis302dl.h:
-/Users/charlesmurphy/STM32CubeIDE/workspace_1.18.0/Demonstrations/Drivers/BSP/Components/lis302dl/../Common/accelero.h:
--- STM32CubeIDE/Debug/Drivers/BSP/Components/lis3dsh.d
+++
@@ -1,6 +0,0 @@
-Drivers/BSP/Components/lis3dsh.o: \
- /Users/charlesmurphy/STM32CubeIDE/workspace_1.18.0/Demonstrations/Drivers/BSP/Components/lis3dsh/lis3dsh.c \
- /Users/charlesmurphy/STM32CubeIDE/workspace_1.18.0/Demonstrations/Drivers/BSP/Components/lis3dsh/lis3dsh.h \
- /Users/charlesmurphy/STM32CubeIDE/workspace_1.18.0/Demonstrations/Drivers/BSP/Components/lis3dsh/../Common/accelero.h
-/Users/charlesmurphy/STM32CubeIDE/workspace_1.18.0/Demonstrations/Drivers/BSP/Components/lis3dsh/lis3dsh.h:
-/Users/charlesmurphy/STM32CubeIDE/workspace_1.18.0/Demonstrations/Drivers/BSP/Components/lis3dsh/../Common/accelero.h:
