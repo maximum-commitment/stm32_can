b1c71fe9c56ce17b79cb3ee375caddb4b13d0b4d
maximum-commitment <batman@gotham.gov>
2026-03-29 20:23:07 -0400

    major refactor - removed many unused files

--- Src/main.c
+++ Src/main.c
@@ -18,7 +18,6 @@
 #include "stm32f4xx_hal_pwr.h"
 #include "stm32f4xx_ll_system.h"
 #include "stm32f4xx_hal_exti.h"
-#include "stm32f4_discovery.h"
 
 /*-----------------------------------------------------------*/
 
@@ -30,6 +29,21 @@ static void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_CAN1_Init(void);
 void Error_Handler(void);
+GPIO_TypeDef* GPIO_PORT[LEDn] = {LED4_GPIO_PORT, 
+                                 LED3_GPIO_PORT, 
+                                 LED5_GPIO_PORT,
+                                 LED6_GPIO_PORT};
+const uint16_t GPIO_PIN[LEDn] = {LED4_PIN, 
+                                 LED3_PIN, 
+                                 LED5_PIN,
+                                 LED6_PIN};
+
+const uint8_t BUTTON_IRQn[BUTTONn] = {KEY_BUTTON_EXTI_IRQn};
+
+void BSP_LED_Init(Led_TypeDef Led);
+void BSP_LED_On(Led_TypeDef Led);
+void BSP_LED_Off(Led_TypeDef Led);
+void BSP_LED_Toggle(Led_TypeDef Led);
 
 CAN_HandleTypeDef hcan1;
 
@@ -213,6 +227,75 @@ void Task_LEDFlash_SetupHw()
     BSP_LED_Init(LED4);
     BSP_LED_Init(LED5);
     BSP_LED_Init(LED6);
+}
+
+/**
+  * @brief  Configures LED GPIO.
+  * @param  Led: Specifies the Led to be configured. 
+  *   This parameter can be one of following parameters:
+  *     @arg LED4
+  *     @arg LED3
+  *     @arg LED5
+  *     @arg LED6
+  */
+void BSP_LED_Init(Led_TypeDef Led)
+{
+  GPIO_InitTypeDef  GPIO_InitStruct;
+  
+  /* Enable the GPIO_LED Clock */
+  LEDx_GPIO_CLK_ENABLE(Led);
+
+  /* Configure the GPIO_LED pin */
+  GPIO_InitStruct.Pin = GPIO_PIN[Led];
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FAST;
+  
+  HAL_GPIO_Init(GPIO_PORT[Led], &GPIO_InitStruct);
+  
+  HAL_GPIO_WritePin(GPIO_PORT[Led], GPIO_PIN[Led], GPIO_PIN_RESET); 
+}
+
+/**
+  * @brief  Turns selected LED On.
+  * @param  Led: Specifies the Led to be set on. 
+  *   This parameter can be one of following parameters:
+  *     @arg LED4
+  *     @arg LED3
+  *     @arg LED5
+  *     @arg LED6  
+  */
+void BSP_LED_On(Led_TypeDef Led)
+{
+  HAL_GPIO_WritePin(GPIO_PORT[Led], GPIO_PIN[Led], GPIO_PIN_SET); 
+}
+
+/**
+  * @brief  Turns selected LED Off.
+  * @param  Led: Specifies the Led to be set off. 
+  *   This parameter can be one of following parameters:
+  *     @arg LED4
+  *     @arg LED3
+  *     @arg LED5
+  *     @arg LED6 
+  */
+void BSP_LED_Off(Led_TypeDef Led)
+{
+  HAL_GPIO_WritePin(GPIO_PORT[Led], GPIO_PIN[Led], GPIO_PIN_RESET); 
+}
+
+/**
+  * @brief  Toggles the selected LED.
+  * @param  Led: Specifies the Led to be toggled. 
+  *   This parameter can be one of following parameters:
+  *     @arg LED4
+  *     @arg LED3
+  *     @arg LED5
+  *     @arg LED6  
+  */
+void BSP_LED_Toggle(Led_TypeDef Led)
+{
+  HAL_GPIO_TogglePin(GPIO_PORT[Led], GPIO_PIN[Led]);
 }
 
 /**
